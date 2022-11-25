0fb16dcfec56950059ad830ef24a3cb75496d0d7
fedor <fedyaq1@gmail.com>
2022-11-25 16:15:11 +0200

    Initial commit

---
+++ src/main/java/com/example/wordsfromfilm/WordsFromFilmApplication.java
@@ -0,0 +1,13 @@
+package com.example.wordsfromfilm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordsFromFilmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordsFromFilmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wordsfromfilm/WordsFromFilmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wordsfromfilm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordsFromFilmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
